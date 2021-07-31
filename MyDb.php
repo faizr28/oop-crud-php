1236e9823e60abc3d590b1bb8a51f0be7ab7a1d2
aryahdyt <aryapakehidayat@gmail.com>
2021-07-31 11:20:58 +0700

    menambahkan inharitance

--- MyDb.php
+++ MyDb.php
@@ -11,44 +11,4 @@ class MyDb
         // metode PDO
         $this->db = new PDO("mysql:host={$host};dbname={$dbname}", $username, $password);
     }
-
-    public function indexWarga()
-    {
-        $query = $this->db->prepare("SELECT * FROM data_warga");
-        $query->execute();
-        $data = $query->fetchAll();
-
-        return $data;
-    }
-
-    public function showWarga($id_warga)
-    {
-        $query = $this->db->prepare("SELECT * FROM data_warga where id=?");
-        $query->bindParam(1, $id_warga);
-        $query->execute();
-
-        return $query->fetch();
-    }
-
-    public function storeWarga($no_ktp, $nama_lengkap, $alamat, $no_hp)
-    {
-        $data = $this->db->prepare('INSERT INTO data_warga (no_ktp,nama_lengkap,alamat,no_hp) VALUES (?, ?, ?, ?)');
-
-        $data->bindParam(1, $no_ktp);
-        $data->bindParam(2, $nama_lengkap);
-        $data->bindParam(3, $alamat);
-        $data->bindParam(4, $no_hp);
-        $data->execute();
-
-        return $data->rowCount();
-    }
-
-    public function destroyWarga($id_warga)
-    {
-        $query = $this->db->prepare("DELETE FROM data_warga where id=?");
-        $query->bindParam(1, $id_warga);
-        $query->execute();
-
-        return $query->rowCount();
-    }
 }
